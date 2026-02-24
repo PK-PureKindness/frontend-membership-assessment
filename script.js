6af3f2b793a136018f69481ec38eef9a88cc71ac
PK-PUREKINDNESS <162641527+PK-PureKindness@users.noreply.github.com>
2026-02-24 15:29:32 +0300

    Add files via upload
    finished the front end

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleFoundation(){
+
+    let text=document.getElementById("foundationText");
+    
+    text.style.display=
+    text.style.display==="block"?"none":"block";
+    
+    }
+    
+    
+    function toggleEconomy(){
+    
+    let text=document.getElementById("economyText");
+    
+    text.style.display=
+    text.style.display==="block"?"none":"block";
+    
+    }
